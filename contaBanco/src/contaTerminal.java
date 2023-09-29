df604e4ca4abad021deb5c2571101c573f283ab4
JoseBeneditoConceicao <josebeneditoconceicao@gmail.com>
2023-09-28 22:33:16 -0400

    desafio

--- contaBanco/src/contaTerminal.java
+++ contaBanco/src/contaTerminal.java
@@ -1,5 +1,23 @@
-public class contaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Digite a conta: ");
+        numero = scanner.nextInt();
+
+        /*System.out.print("Digite a agência: ");
+        String agencia = scanner.next();
+
+        System.out.print("Digite o nome do cliente: ");
+        String nome_cliente = scanner.next();
+
+        System.out.print("Digite o saldo: ");
+        Double saldo = scanner.nextDouble();*/
+
+        System.out.println("Olá " + numero);
+        //"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
     }
 }
